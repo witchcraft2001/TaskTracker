3dc1d9eca5e117b97553d6a78d37dfc3bef6b1a1
DmitryM <mikhaltchenkov@gmail.com>
2022-05-08 17:56:34 +0300

    updated repositories

--- app/src/main/java/com/dmdev/tasktracker/repositories/CategoriesRepository.kt
+++ app/src/main/java/com/dmdev/tasktracker/repositories/CategoriesRepository.kt
@@ -19,12 +19,12 @@ class CategoriesRepository @Inject constructor(
         return flow {
             emit(ResultWrapper.Loading)
             emit(ResultWrapper.Success(listOf(
-                CategoryModel(1, "Shopping", 0xFF808080, CategoryIcon.SHOP),
-                CategoryModel(2, "Gaming", 0xFF909090, CategoryIcon.GAME),
-                CategoryModel(3, "Studying", 0xFF606060, CategoryIcon.STUDY),
-                CategoryModel(4, "Driving", 0xFF505050, CategoryIcon.CAR),
-                CategoryModel(5, "Chilling", 0xFF707070, CategoryIcon.MUSIC),
-                CategoryModel(6, "Building", 0xFFA0A0A0, CategoryIcon.BRUSH)
+                CategoryModel(1, "Shopping", 0xFFEF9A9A, CategoryIcon.SHOP),
+                CategoryModel(2, "Gaming", 0xFFF48FB1, CategoryIcon.GAME),
+                CategoryModel(3, "Studying", 0xFFCE93D8, CategoryIcon.STUDY),
+                CategoryModel(4, "Driving", 0xFFB39DDB, CategoryIcon.CAR),
+                CategoryModel(5, "Chilling", 0xFF9FA8DA, CategoryIcon.MUSIC),
+                CategoryModel(6, "Building", 0xFF90CAF9, CategoryIcon.BRUSH)
             )))
         }.flowOn(ioDispatcher)
     }
--- app/src/main/java/com/dmdev/tasktracker/repositories/TasksRepository.kt
+++ app/src/main/java/com/dmdev/tasktracker/repositories/TasksRepository.kt
@@ -24,7 +24,7 @@ class TasksRepository @Inject constructor(
                         TaskModel(
                             1,
                             "Разработать дизайн программы",
-                            1,
+                            3,
                             Date(2022, 4, 5, 9, 0).time,
                             null,
                             null
@@ -32,9 +32,41 @@ class TasksRepository @Inject constructor(
                         TaskModel(
                             2,
                             "Разработать структуру данных программы",
-                            1,
+                            3,
                             Date(2022, 4, 5, 9, 0).time,
                             Date(2022, 4, 5, 9, 15).time,
+                            null
+                        ),
+                        TaskModel(
+                            3,
+                            "Поклеить обои",
+                            6,
+                            Date(2022, 3, 10, 9, 0).time,
+                            Date(2022, 3, 10, 10, 15).time,
+                            null
+                        ),
+                        TaskModel(
+                            4,
+                            "Сходить на концерт",
+                            5,
+                            Date(2022, 3, 7, 11, 30).time,
+                            Date(2022, 3, 7, 15, 45).time,
+                            null
+                        ),
+                        TaskModel(
+                            5,
+                            "Помыть машину",
+                            4,
+                            Date(2022, 3, 15, 9, 0).time,
+                            Date(2022, 3, 15, 10, 15).time,
+                            null
+                        ),
+                        TaskModel(
+                            6,
+                            "Поиграть в настолки",
+                            2,
+                            Date(2022, 2, 20, 11, 30).time,
+                            Date(2022, 2, 20, 15, 45).time,
                             null
                         )
                     )
