dd360cf1d44018908e1edc9e24773df0c220dd14
DmitryM <mikhaltchenkov@gmail.com>
2022-05-19 09:04:28 +0300

    fixed tests

--- app/src/test/java/com/dmdev/tasktracker/usecases/GetAllCategoriesUseCaseUnitTest.kt
+++ app/src/test/java/com/dmdev/tasktracker/usecases/GetAllCategoriesUseCaseUnitTest.kt
@@ -3,6 +3,7 @@ package com.dmdev.tasktracker.usecases
 import com.dmdev.tasktracker.data.ResultWrapper
 import com.dmdev.tasktracker.data.entities.CategoryEntity
 import com.dmdev.tasktracker.data.CategoryIcon
+import com.dmdev.tasktracker.data.Colors
 import com.dmdev.tasktracker.data.domain.Category
 import com.dmdev.tasktracker.repositories.CategoriesRepository
 import kotlinx.coroutines.flow.Flow
@@ -16,9 +17,17 @@ import org.junit.Test
 class FakeCategoriesRepository : CategoriesRepository {
     override suspend fun getAllCategories(): Flow<ResultWrapper<List<CategoryEntity>>> = flow {
         emit(ResultWrapper.Success(listOf(
-            CategoryEntity(1, "Test", 0, CategoryIcon.BROOM),
-            CategoryEntity(2, "Test2", 1, CategoryIcon.BROOM)
+            CategoryEntity(1, "Test", Colors.CIAN.value.value.toLong(), CategoryIcon.BROOM),
+            CategoryEntity(2, "Test2", Colors.AMBER.value.value.toLong(), CategoryIcon.BROOM)
         )))
+    }
+
+    override suspend fun add(category: CategoryEntity) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun update(category: CategoryEntity) {
+        TODO("Not yet implemented")
     }
 }
 
@@ -28,13 +37,17 @@ class GetAllCategoriesUseCaseUnitTest {
 
     @Test
     fun `UseCase return the same items mapped to the domain model`() = runBlocking {
+        //Arrange
+
+        //Act
         val result = useCase.execute().toList()
 
+        //Assert
         assertTrue(result.last() is ResultWrapper.Success<*>)
 
         val expectedList = listOf(
-            Category(1, "Test", 0, CategoryIcon.BROOM),
-            Category(2, "Test2", 1, CategoryIcon.BROOM)
+            Category(1, "Test", Colors.CIAN, CategoryIcon.BROOM),
+            Category(2, "Test2", Colors.AMBER, CategoryIcon.BROOM)
         )
         assertEquals(expectedList, (result.last() as ResultWrapper.Success).result)
     }
--- app/src/test/java/com/dmdev/tasktracker/usecases/GetAllTasksUseCaseUnitTest.kt
+++ app/src/test/java/com/dmdev/tasktracker/usecases/GetAllTasksUseCaseUnitTest.kt
@@ -3,6 +3,7 @@ package com.dmdev.tasktracker.usecases
 import com.dmdev.tasktracker.data.ResultWrapper
 import com.dmdev.tasktracker.data.entities.CategoryEntity
 import com.dmdev.tasktracker.data.CategoryIcon
+import com.dmdev.tasktracker.data.Colors
 import com.dmdev.tasktracker.data.entities.PeriodEntity
 import com.dmdev.tasktracker.data.entities.TaskEntity
 import com.dmdev.tasktracker.data.domain.Category
@@ -28,7 +29,7 @@ class GetAllTasksUseCaseUnitTest {
 
     @Test
     fun `UseCase return the same items mapped to the domain model`() = runBlocking {
-
+        //Arrange
         Mockito.`when`(mockTasksRepository.getAllTasks()).thenReturn(flow {
             emit(
                 ResultWrapper.Success(
@@ -44,8 +45,8 @@ class GetAllTasksUseCaseUnitTest {
             emit(
                 ResultWrapper.Success(
                     listOf(
-                        CategoryEntity(1, "Category1", 0, CategoryIcon.BROOM),
-                        CategoryEntity(2, "Category2", 1, CategoryIcon.CALL)
+                        CategoryEntity(1, "Category1", Colors.CIAN.value.value.toLong(), CategoryIcon.BROOM),
+                        CategoryEntity(2, "Category2", Colors.AMBER.value.value.toLong(), CategoryIcon.CALL)
                     )
                 )
             )
@@ -63,18 +64,21 @@ class GetAllTasksUseCaseUnitTest {
         })
 
         val useCase = GetAllTasksUseCase(mockTasksRepository, mockCategoriesRepository, mockPeriodsRepository)
+
+        //Act
         val result = useCase.execute().toList()
 
+        //Assert
         assertTrue(result.last() is ResultWrapper.Success<*>)
 
         val expectedList = listOf(
             Task(
-                1, "Task1", Category(2, "Category2", 1, CategoryIcon.CALL), 0, 5, null, listOf(
+                1, "Task1", Category(2, "Category2", Colors.AMBER, CategoryIcon.CALL), 0, 5, null, listOf(
                     Period(1, 1, 0, 5)
                 )
             ),
             Task(
-                2, "Task2", Category(1, "Category1", 0, CategoryIcon.BROOM), 10, null, null, listOf(
+                2, "Task2", Category(1, "Category1", Colors.CIAN, CategoryIcon.BROOM), 10, null, null, listOf(
                     Period(2, 2, 10, null)
                 )
             ),
